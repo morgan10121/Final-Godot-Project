f576dddc0911a5b39593c8470866889c9c639dd9
morgan10121 <132925202+morgan10121@users.noreply.github.com>
2023-06-07 19:07:57 -0600

    animation added

--- Character.gd
+++ Character.gd
@@ -1,55 +1,52 @@
 extends KinematicBody2D
 
 onready var sprite := $AnimatedSprite
-const SPEED := 400.0
+const SPEED := 250.0
 const HEALTH := 100.0
 const DAMAGE := 5.0
 
-var characterDir = "idle"
+var characterDir = "right"
+var isAttacking = false
 
 func _physics_process(delta: float) -> void:
-
-	var direction := Vector2.ZERO
-
-	if Input.is_action_pressed("move_right"):
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if Input.is_action_pressed("move_right") and isAttacking == false:
 		sprite.flip_h = false
 		characterDir = "right"
 		sprite.play("Run")
-		direction.x = 1.0
-
-	elif Input.is_action_pressed("move_left"):
+		
+	elif Input.is_action_pressed("move_left") and isAttacking == false:
 		sprite.flip_h = true
 		characterDir = "left"
 		sprite.play("Run")
-		direction.x = -1.0
-
-	elif Input.is_action_pressed("move_up"):
-		direction.y = -1.0
-		if characterDir == "right":
-			sprite.flip_h = false
-			sprite.play("Run")
-		if characterDir == "left":
-			sprite.flip_h = true
-			sprite.play("Run")
+		
+	elif Input.is_action_pressed("move_up") and isAttacking == false:
+		sprite.play("Run")
+		
+	elif Input.is_action_pressed("move_down") and isAttacking == false:
+		sprite.play("Run")
 	
-	elif Input.is_action_pressed("move_right") and Input.is_action_pressed("move_up"):
-		direction.x = 1
-		direction.y = 1
-
-	elif Input.is_action_pressed("move_down"):
-		
-		direction.y = 1.0
-		
 	else:
-		$AnimatedSprite.play("Idle")
-		characterDir = "idle"
-		
+		if isAttacking == false:
+			sprite.play("Idle")
+			
+	if Input.is_action_just_pressed("attack1"):
+		sprite.play("Attack")
+		isAttacking = true
+		if characterDir == "right":
+			$AttackArea/CollisionShape2D.disabled = false
+		if characterDir == "left":
+			$AttackArea/CollisionShape2D2.disabled = false
+
 	var velocity := direction * SPEED
 	move_and_slide(velocity)
-	
-	var direction_key = direction.round()
-	direction_key.x = abs(direction_key.x)
 
-
+func _on_AnimatedSprite_animation_finished():
+	if sprite.animation == "Attack" and characterDir == "right":
+		$AttackArea/CollisionShape2D.disabled = true
+		isAttacking = false
 		
-	
+	elif sprite.animation == "Attack" and characterDir == "left":
+		$AttackArea/CollisionShape2D2.disabled = true
+		isAttacking = false
