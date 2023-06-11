44a312233cfc567b0cf1e48726d4cafb1e352a9d
morgan10121 <132925202+morgan10121@users.noreply.github.com>
2023-06-11 12:56:22 -0600

    menu

--- Controls.gd
+++ Controls.gd
@@ -10,15 +10,11 @@ extends Control
 func _ready():
 	pass # Replace with function body.
 
-func _on_Start_pressed():
-	get_tree().change_scene("res://Map/Scene1.tscn")
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
 
 
 func _on_Quit_pressed():
-	get_tree().quit()
-
-
-
-func _on_Options_pressed():
-	get_tree().change_scene("res://Controls.tscn")
+		get_tree().change_scene("res://Menu.tscn")
